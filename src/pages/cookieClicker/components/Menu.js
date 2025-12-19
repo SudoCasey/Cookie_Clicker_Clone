818601a638d53294a3dc8c4dd8050f7d7e465118
CaseyADASC <Casey@adasitecompliance.com>
2025-12-18 23:55:57 -0500

    WIP: Menu modal

--- src/pages/cookieClicker/components/Menu.js
+++ src/pages/cookieClicker/components/Menu.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
-import styles from '@/styles/Navbar.module.scss';
+import styles from '@/styles/Menu.module.scss';
 import Dialog from '@mui/material/Dialog';
 //import Image from 'next/image';
 
     
 export default function Menu(props){
-    <section>
-        <p>test</p>
-    </section>
+
+
+    return (
+        <section className={`${styles.menu}`}>
+            <p>test</p>
+        </section>
+    );
+
 }
